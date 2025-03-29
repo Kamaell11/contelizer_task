f629b006ebe0ae53687c20ccb26b87ea0ba5ce54
Kamil Skowronek <kamilskylark@gmail.com>
2025-03-29 20:49:31 +0100

    Add media file handling and text processing functionality
    - Update .gitignore to exclude media directory
    - Configure MEDIA_URL and MEDIA_ROOT in settings
    - Implement text processing view to scramble words from uploaded text files
    - Create upload and result templates for text processing
    - Add URL routing for text processing app

--- contelizer/text_processor/views.py
+++ contelizer/text_processor/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+import random
+
+def scramble_word(word):
+    if len(word) > 3:
+        middle = list(word[1:-1])
+        random.shuffle(middle)
+        return word[0] + ''.join(middle) + word[-1]
+    return word
+
+def process_text(request):
+    if request.method == 'POST' and request.FILES['text_file']:
+        file = request.FILES['text_file']
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        with fs.open(filename) as f:
+            content = f.read().decode('utf-8')
+            words = content.split()
+            scrambled_text = ' '.join(scramble_word(word) for word in words)
+        return render(request, 'text_result.html', {'text': scrambled_text})
+    return render(request, 'upload.html')
